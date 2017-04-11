4239ace956a08c8405d2c0482cb3a6b696b633dd
eatingbird <consulthere@gmail.com>
2017-04-10 19:10:39 -0700

    Google Map code added

---
+++ static/js/map.js
@@ -0,0 +1,112 @@
+function initMap() {
+    console.log('in initMap');
+
+    var irvine = {lat: 33.681902, lng: -117.852098};
+    
+    var map = new google.maps.Map(document.getElementById('map'),{
+                center: irvine,
+                zoom: 2,
+                // Map style from
+                // https://snazzymaps.com/style/100182/new-artium
+                styles: [
+                        {
+                            "featureType": "administrative",
+                            "elementType": "labels.text.fill",
+                            "stylers": [
+                                {
+                                    "color": "#444444"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "landscape",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "color": "#f2f2f2"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "saturation": -100
+                                },
+                                {
+                                    "lightness": 45
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.highway",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "visibility": "simplified"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.arterial",
+                            "elementType": "labels.icon",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "transit",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "water",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "color": "#cdd2d4"
+                                },
+                                {
+                                    "visibility": "on"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "water",
+                            "elementType": "geometry.fill",
+                            "stylers": [
+                                {
+                                    "color": "#dfdbd8"
+                                }
+                            ]
+                        }
+                    ]
+              });
+
+    var marker = new google.maps.Marker({
+                position: irvine,
+                map: map
+    });
+
+}
+
+
+
+
+// google.maps.event.addDomListener(window, 'load', initMap);
